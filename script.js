cebfebfdfcbce3529d1d753103b8a24d2cdcbd6e
ELIANE RAMOS BARBOSA <137837171+elianerbarbosa@users.noreply.github.com>
2023-10-04 21:24:35 -0300

    Add files via upload
    Trabalho de conclusão do curso. A proposta que pudesse colocar todo o aprendizado em prática com elaboração de um site com todos os recursos vistos e exercitados em aulas. Foi realizado a utilização de formulário, aplicação de imagens, animação utilizando as ferramentas(html, css, javascript) ensinadas durante todo o período do curso.

---
+++ script.js
@@ -0,0 +1,22 @@
+function ativaLetra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i)=>{
+setTimeout(()=>{
+       elemento.innerHTML += Letra;
+}, 75 * i);
+});
+}
+
+const titulo = document.querySelector('.digitando');
+ativaLetra(titulo);
+
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria')
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+});
+
+
